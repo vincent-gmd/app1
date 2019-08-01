b49c1f69292026d843256ecc0a1eb18f9291169b
vincent gabriel maris dafre <vinvinvinvin123@gmail.com>
2019-07-31 22:36:57 -0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication1/MainControl.java
@@ -0,0 +1,14 @@
+package com.example.myapplication1;
+
+import android.app.Activity;
+
+public class MainControl {
+    private Activity activity;
+
+    public MainControl(Activity activity){
+
+        this.activity =activity;
+
+    }
+
+}
